f81d929bae62ee18acbf232889dcece8eb0b634e
RejoiceOwoh <owohrejoice@gmail.com>
2024-09-21 20:48:40 +0100

    Made some changes to the favicon and the Hero Logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -56,7 +56,7 @@ export default function Header() {
           }`}
       >
         <nav
-          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+          className="mx-auto flex w-full items-center justify-between p-6 lg:px-8"
           aria-label="Global"
         >
           <div className="flex lg:flex-1">
