b07058ab65994892166e54ac194b238ab386632b
RejoiceOwoh <owohrejoice@gmail.com>
2024-09-25 13:34:00 +0100

    corrections

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
     images: {
       domains: ['res.cloudinary.com','www.allrecipes.com', 'www.chilipeppermadness.com'],
     },
-    output: 'export',
   };
 
 export default nextConfig;
