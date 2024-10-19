26a86d3faa65845aadc7ea632fe977f4de20bdc1
RejoiceOwoh <owohrejoice@gmail.com>
2024-10-19 22:11:36 +0100

    Changed the admin package to acefoods logo

--- src/app/admin/components/main-nav.tsx
+++ src/app/admin/components/main-nav.tsx
@@ -9,6 +9,7 @@ import {
     ShoppingCart,
     Users2,
   } from "lucide-react"
+  import Image from "next/image"
 
 import { cn } from "../../../lib/utils"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
@@ -25,7 +26,13 @@ export function AdminNav({
             href="#"
             className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
           >
-            <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
+            <Image
+              src="/logo.png"
+              alt="Afrigold Logo"
+              width={60}
+              height={60}
+              className="w-full h-full object-cover transition-all group-hover:scale-110"
+            />
             <span className="sr-only">Acefoods Global</span>
           </Link>
           <TooltipProvider>
